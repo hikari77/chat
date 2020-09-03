819dcf76252d12f5714353f3738bf003cdc7cc25
RIGUANG JIN <bosco@RIGUANGs-iMac.local>
2020-09-03 13:21:11 -0400

    setup react

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss'
+import { Container } from 'react-bootstrap'
+import Register from './pages/Register';
+
+function App() {
+  
+  return (
+    <Container className="pt-5">
+      <Register />
+    </Container>
+  );
+}
+
+export default App;
