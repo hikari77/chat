a064ca28a19268b842dae442c4644c50f9ea15af
RIGUANG JIN <bosco@RIGUANGs-iMac.local>
2020-09-06 11:32:43 -0400

    auth context#9

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Row, Col, Form, Button } from 'react-bootstrap';
 import { gql, useLazyQuery } from '@apollo/client';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+
+import { useAuthDispatch } from '../context/auth';
 
 const LOGIN_USER = gql`
     query login(
@@ -28,10 +30,13 @@ export default function Register(props) {
       })
     const [errors, setErrors] = useState({})
 
+    const dispatch = useAuthDispatch()
+
     const [loginUser, { loading }] = useLazyQuery(LOGIN_USER, {
         onError: (err) => setErrors(err.graphQLErrors[0].extensions.errors),
         onCompleted(data){
-            localStorage.setItem('token', data.login.token);
+            
+            dispatch({ type: 'LOGIN', payload: data.login })
             props.history.push('/')
         }
     })
